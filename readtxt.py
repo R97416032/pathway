ac95f9b1a99c46e259795e8e685312d983d96834
Monster-R <97416032@qq.com>
2023-04-25 11:33:33 +0800

    优化了read，生成的文件名更易读

--- readtxt.py
+++ readtxt.py
@@ -15,18 +15,24 @@ for line in lines:
     data=line.strip('\n').split()
     index=data[0].strip()
     if(re.split('[0-9]',index)[0]=="A"):
-        str=""
-        nameA=index[1:]+" "+str.join(data[1:]).replace(": "," ")
+        nameA=""
+        for i in data[1:]:
+            nameA=nameA+" "+i
+        nameA=index[1:]+" "+nameA.replace(": "," ")
         if not os.path.exists("./data/"+nameA):
             os.makedirs("./data/"+nameA)
     elif(re.split('[0-9]',index)[0]=="B"):
-        str = ""
-        nameB = str.join(data[1:]).replace(": "," ")
+        nameB = ""
+        for i in data[2:]:
+            nameB=nameB+" "+i
+        nameB = data[1]+nameB.replace(": "," ")
         if not os.path.exists("./data/" + nameA+"/"+nameB):
             os.makedirs("./data/" + nameA+"/"+nameB)
     elif(re.split('[0-9]',index)[0]=="C"):
-        str = ""
-        nameC="has"+ str.join(data[1:]).replace("："," ")
+        nameC = ""
+        for i in data[2:]:
+            nameC=nameC+" "+i
+        nameC="has"+data[1] +nameC.replace("："," ")
         nameC=nameC.replace("/","")
         nameC=nameC.replace("-","")
         if nameC.find('[')>-1:
