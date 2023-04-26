c2cada60732f2af1e3b8408fc20f9391b6067528
R <97416032@qq.com>
2023-04-26 17:36:08 +0800

    生成的通路csv和代码

--- dataProcess.py
+++ dataProcess.py
@@ -1,19 +1,70 @@
 import os
 import pandas as pd
-
+import csv
 rootpath="./data/"
-#生成每个次类别中通路中的基因频次表，行为通路名，列为基因名
+#生成每个B类别中通路中的基因频次表，行为通路名，列为基因名
 listA=os.listdir(rootpath)[0:-2]
-print(listA)
 for i in listA:
     path=rootpath+i+'/'
     listB=os.listdir(path)
     pathways = []
-    genenames = []
-    for j in listB:
 
+    for j in listB:
+        genenames = ['pathway']
+        pathC=path+j+'/'
+        filenames=os.listdir(pathC)
+        dictB={}
+        for k in filenames:
+            pathways.append(k[:-4])
+            genefile=open(pathC+k,'r')
+            temp=[]
+            for g in genefile.readlines():
+                genenames.append(g.strip('\n'))
+                temp.append(g.strip('\n'))
+            dictB[k[:-4]]=temp
+        genenames=sorted(set(genenames),key=genenames.index)
+        if not os.path.exists("./csv/"+i+'/'+j ):
+            os.makedirs("./csv/"+i+'/'+j)
+        with open("./csv/"+i+'/'+j+"/"+j+".csv", "w",newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(genenames)
+            for key in dictB.keys():
+                temp=[key]
+                for g in genenames[1:]:
+                    if g in dictB[key]:
+                        temp.append(1)
+                    else:
+                        temp.append((0))
+                writer.writerow(temp)
+#生成每个A类别中通路中的基因频次表，行为通路名，列为基因名
+for i in listA:
+    path=rootpath+i+'/'
+    listB=os.listdir(path)
+    pathways = []
+    genenames = ['pathway']
+    dictB = {}
+    for j in listB:
         pathC=path+j+'/'
         filenames=os.listdir(pathC)
         for k in filenames:
             pathways.append(k[:-4])
-        print(pathways)
+            genefile=open(pathC+k,'r')
+            temp=[]
+            for g in genefile.readlines():
+                genenames.append(g.strip('\n'))
+                temp.append(g.strip('\n'))
+            dictB[k[:-4]]=temp
+    genenames=sorted(set(genenames),key=genenames.index)
+    if not os.path.exists("./csv/"+i ):
+        os.makedirs("./csv/"+i)
+    with open("./csv/"+i+'/'+i+".csv", "w",newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(genenames)
+        for key in dictB.keys():
+            temp=[key]
+            for g in genenames[1:]:
+                if g in dictB[key]:
+                    temp.append(1)
+                else:
+                    temp.append((0))
+            writer.writerow(temp)
